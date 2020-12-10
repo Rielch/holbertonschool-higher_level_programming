ac3d6f7b948a87c2e543883d357da7f770d4f0ee
Gabriel Prochnau <2288@holbertonschool.com>
2020-12-10 18:51:51 +0000

    Fix PEP8 in part 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-    from sys import argv, exit
+    from sys import argv
 
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
     a = int(argv[1])
-    b= int(argv[3])
+
+    b = int(argv[3])
 
     if argv[2] == "+":
         print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
