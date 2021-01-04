76f9009fc10f8dab650c4c18db13982f78e741cf
Gabriel Prochnau <2288@holbertonschool.com>
2021-01-04 18:36:53 +0000

    Fix part 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
         print("", end="\n")
         return x
-    except IndexError:
+    except:
         print("", end="\n")
         return i
