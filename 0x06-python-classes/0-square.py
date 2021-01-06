6ebb55c9e4b3a5fa4d9570512b72635d510c2c6d
Gabriel Prochnau <2288@holbertonschool.com>
2021-01-06 01:04:35 +0000

    Add README.md, fix documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+"""class Square: Creates a square class"""
 
 class Square:
+    """Creates an empty Square class"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
+"""class Square: Creates a Square class"""
 
 class Square:
     """Defines a square"""
-    def __init__(self, size=0):
-        """Defines the size of the square
-        __init__:
-        size -- size of the square
-        """
+    def __init__(self, size):
+        """Defines the private size of the square"""
         self.__size = size
