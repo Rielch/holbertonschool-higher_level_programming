f1e2721c6f1b24f346d63c94ce0e89f286c4be53
Gabriel Prochnau <2288@holbertonschool.com>
2021-01-12 00:24:59 +0000

    Fix part 7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -82,8 +82,8 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             return ""
 
-        return (((Rectangle.print_symbol * self.__width) + "\n") *\
-                (self.__height - 1)) + (Rectangle.print_symbol * self.__width)
+        return ((Rectangle.print_symbol * self.__width) + "\n")\
+            * (self.__height - 1) + (Rectangle.print_symbol * self.__width)
 
     def __repr__(self):
         """determines the repr method"""
