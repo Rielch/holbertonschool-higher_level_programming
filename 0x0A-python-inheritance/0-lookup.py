ee47978fcd2d11e1b3aeda02caf3fe7a3a19d709
Gabriel Prochnau <2288@holbertonschool.com>
2021-01-19 15:55:00 +0000

    Add part 0 and README.md

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Returns a list of all the available attributes and methods of an object"""
+
+
+
+def lookup(obj):
+    """Return a list of al the available attributes and methods of an object"""
+
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+Gabriel Prochnau`s directory for "Project: 0x0A. Python - Inheritance" from Holberton School
