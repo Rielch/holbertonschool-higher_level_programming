f8e5952d4cef84e327694ead461f1e734c8fea11
Gabriel Prochnau <2288@holbertonschool.com>
2021-01-20 00:44:52 +0000

    Fix PEP8 for part 100

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,12 +7,15 @@ class MyInt(int):
     def __init__(self, value):
         """Initializates a MyInt class"""
         self.value = value
+
     def __new__(cls, value):
         """Creates a new int object"""
         return int.__new__(cls, value)
+
     def __eq__(self, other):
         """defines equality"""
         return self.value != other
+
     def __ne__(self, other):
         """defines inequality"""
         return self.value == other
