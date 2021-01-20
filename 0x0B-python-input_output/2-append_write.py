5fdee03c0d5cedf1bebe4eaab93761f9af8de99e
Gabriel Prochnau <2288@holbertonschool.com>
2021-01-20 20:44:45 +0000

    Add part 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """Writes a string into a file"""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """Writes a string into a file"""
     with open(filename, mode="a", encoding="utf-8") as wr_file:
         wr_file.write(text)
