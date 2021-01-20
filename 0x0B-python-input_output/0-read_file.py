674985277d8f5d377bd3ae3a9ec5fbb1a1c76743
Gabriel Prochnau <2288@holbertonschool.com>
2021-01-20 20:20:42 +0000

    Fix part 0 3rd time

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,5 @@ def read_file(filename=""):
     """Reads a text file and prints it to standard output"""
 
     with open(filename, encoding="utf-8") as rd_file:
-        print(rd_file)
+        for line in rd_file:
+            print(line, end="")
