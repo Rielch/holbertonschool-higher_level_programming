26da9008bb5b2c21e9f44116f35d44b639ee96b7
Gabriel Prochnau <2288@holbertonschool.com>
2021-01-21 00:47:13 +0000

    Fix PEP8 part 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Add all arguments to a python list and then saves it into a file"""
+from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-from sys import argv
 
 
 my_list = argv[:]
