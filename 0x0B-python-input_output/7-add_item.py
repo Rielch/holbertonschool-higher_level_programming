84d61749ae1eb3405713677a2552d6a702f50e75
Gabriel Prochnau <2288@holbertonschool.com>
2021-01-21 00:44:36 +0000

    Add part 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Add all arguments to a python list and then saves it into a file"""
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+from sys import argv
+
+
+my_list = argv[:]
+my_list.pop(0)
+try:
+    my_list = load_from_json_file("add_item.json") + my_list
+except:
+    pass
+save_to_json_file(my_list, "add_item.json")
