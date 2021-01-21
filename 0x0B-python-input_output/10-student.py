fe6853ad70805d04a8d0d33313fc695d19f2d9fd
Gabriel Prochnau <2288@holbertonschool.com>
2021-01-21 01:25:45 +0000

    Add part 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Creates a class Student"""
+
+
+class Student:
+    """Creates a class Student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializates the instance of Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns the dictionary representation of a Student instance"""
+        i = 0
+        new_dict = {}
+        if type(attrs) is list:
+            for element in attrs:
+                if type(element) is not str:
+                    i += 1
+            if a == 0:
+                for element in attrs:
+                    if element in self.__dict__:
+                        new_dict[element] = self.__dict__[element]
+                return new_dict
+        return self.__dict__
