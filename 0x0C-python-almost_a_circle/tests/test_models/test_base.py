a8ab410df5829d76d24fb2ed84d7f17a5e1d3b07
Gabriel Prochnau <2288@holbertonschool.com>
2021-01-26 21:54:55 +0000

    Added test cases for rectangles module

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -5,9 +5,6 @@ from models import base
 
 class BaseTest(unittest.TestCase):
 
-        b1 = base.Base()
-        b2 = base.Base(98)
-
         def test_doc(self):
                 self.assertTrue(len(base.__doc__) > 1)
                 self.assertTrue(len(base.Base.__doc__) > 1)
@@ -19,6 +16,8 @@ class BaseTest(unittest.TestCase):
                 self.assertTrue(len(base.Base.load_from_file.__doc__) > 1)
 
         def test_Base(self):
+                b1 = base.Base()
+                b2 = base.Base(98)
                 self.assertNotEqual(b1, b2)
                 self.assertEqual(b1.id, 1)
                 self.assertEqual(b2.id, 98)
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,6 +1,26 @@
 #!/usr/bin/python3
 from models import rectangle
+import unittest
 
 
-def Testrectangle(unittest.TestCase):
-    
+class RectangleTest(unittest.TestCase):
+
+    def test_doc(self):
+        self.assertTrue(len(rectangle.__doc__) > 1)
+        self.assertTrue(len(rectangle.Rectangle.__doc__) > 1)
+        self.assertTrue(len(rectangle.Rectangle.__init__.__doc__) > 1)
+        self.assertTrue(len(rectangle.Rectangle.area.__doc__) > 1)
+        self.assertTrue(len(rectangle.Rectangle.display.__doc__) > 1)
+        self.assertTrue(len(rectangle.Rectangle.update.__doc__) > 1)
+        self.assertTrue(len(rectangle.Rectangle.__str__.__doc__) > 1)
+        self.assertTrue(len(rectangle.Rectangle.to_dictionary.__doc__) > 1)
+
+    def test_Rectangle(self):
+        self.assertRaises(TypeError, rectangle.Rectangle("2", 3))
+        self.assertRaises(ValueError, rectangle.Rectangle(-2, 0))
+        self.assertRaises(TypeError, rectangle.Rectangle(2, 2, "2"))
+        self.assertRaises(ValueError, rectangle.Rectangle(2, 2, 2, -1))
+        self.assertRaises(
+
+if __name__ == "__main__":
+    unittest.main()
