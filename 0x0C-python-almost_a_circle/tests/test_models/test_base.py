d736d87a0345af070a349a01822cad0830bed428
Gabriel Prochnau <2288@holbertonschool.com>
2021-01-26 02:55:37 +0000

    Add test cases for base

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import unittest
+from models import base
+
+
+class BaseTest(unittest.TestCase):
+
+        b1 = base.Base()
+        b2 = base.Base(98)
+
+    def test_doc(self):
+        self.assertTrue(len(base.__doc__) > 1)
+        self.assertTrue(len(base.Base.__doc__) > 1)
+        self.assertTrue(len(base.Base.__init__.__doc__) > 1)
+        self.assertTrue(len(base.Base.to_json_string.__doc__) > 1)
+        self.assertTrue(len(base.Base.save_to_file.__doc__) > 1)
+        self.assertTrue(len(base.Base.from_json_string.__doc__) > 1)
+        self.assertTrue(len(base.Base.create.__doc__) > 1)
+        self.assertTrue(len(base.Base.load_from_file.__doc__) > 1)
+
+    def test_Base(self):
+        self.assertNotEqual(b1, b2)
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 98)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+from models import rectangle
+
+
+def Testrectangle(unittest.TestCase):
+    
