0e06abbbb2ac9967f505f2ea98a79ce3ada8e6e4
Gabriel Prochnau <2288@holbertonschool.com>
2021-03-03 00:04:04 +0000

    Fix part 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- Lists all shows that have at least one genre linked
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_shows_genres.show_id WHERE tv_show_genres.show_id IS NOT NULL ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id WHERE tv_show_genres.show_id IS NOT NULL ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
