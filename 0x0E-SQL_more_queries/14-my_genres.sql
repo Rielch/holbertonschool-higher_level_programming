4af59ab93134ebbf3033af958aff874ba388edb6
Gabriel Prochnau <2288@holbertonschool.com>
2021-03-03 02:59:25 +0000

    Fix 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,2 +1,2 @@
 -- Lists all genres of the show Dexter
-SELECT tv_genres.name AS name FROM tv_genres INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id WHERE tv_shows.title = "Dexter";
+SELECT tv_genres.name AS name FROM tv_genres INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id WHERE tv_shows.title = "Dexter" ORDER BY tv_genre.name ASC;
