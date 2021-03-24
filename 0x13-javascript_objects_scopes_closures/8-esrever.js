67b8e98afa4b3c2de91078839373241f7e72cfcd
Gabriel Prochnau <2288@holbertonschool.com>
2021-03-24 00:31:18 +0000

    Fix semistandard for part 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 exports.esrever = function (list) {
   const size = list.length - 1;
   for (let i = 0; i < (size / 2); i++) {
-    let temp = list[i];
+    const temp = list[i];
     list[i] = list[size - i];
     list[size - i] = temp;
   }
