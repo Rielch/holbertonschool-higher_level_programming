7cbb642ef687f0a5c7083f1205892be9cf29ca21
Gabriel Prochnau <2288@holbertonschool.com>
2021-03-29 00:17:42 +0000

    Fix part 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -9,10 +9,9 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(argv[1], argv[2], argv[3]))
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state == session.query(State).order_by(State.id).first()
+    state = session.query(State).order_by(State.id).first()
     if state:
         print('{}: {}'.format(state.id, state.name))
     else:
