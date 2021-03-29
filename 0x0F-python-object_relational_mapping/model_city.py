0d32a97d091df98cc7c11d1ef884200d5b26ba1b
Gabriel Prochnau <2288@holbertonschool.com>
2021-03-29 01:38:23 +0000

    Fix part 14

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Defines the City class"""
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base
 
