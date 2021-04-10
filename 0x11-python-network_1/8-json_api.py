43743b5c00c31345bde295b488bdf526b93d0243
Gabriel Prochnau <2288@holbertonschool.com>
2021-04-10 02:35:20 +0000

    Add all parts

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        dic = {'q': ''}
+    else:
+        dic = {'q': sys.argv[1]}
+
+    r = requests.post('http://0.0.0.0:5000/search_user', data = dic)
+
+    try:
+        json = r.json()
+        if len(json) == 0:
+            print("No result")
+        else:
+            json_id = json.get(id)
+            json_name = json.get(name)
+            print("[<{}>] <{}>".format(json_id, json_name))
+    except ValueError:
+        print("Not a valid JSON")
