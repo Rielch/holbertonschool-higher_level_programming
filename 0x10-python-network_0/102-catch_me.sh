2425bc66ac599d687040498be6c781490173eb98
Rielch <2288@holbertonschool.com>
2021-08-19 23:37:49 +0000

    Add part 102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Makes a request to 0.0.0.0:5000/catch_me and causes the server to respond with You got me!
-curl -d user_id=98 -H 'OrrtonSchool' -L -X PUT 0.0.0.0:5000/catch_me
+curl -d user_id=98 -H 'HolbertonSchool' -L -X PUT 0.0.0.0:5000/catch_me
