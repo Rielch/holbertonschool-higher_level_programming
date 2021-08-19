ee81ec15d37283e55b795d71ab42ea23d05ce24b
Rielch <2288@holbertonschool.com>
2021-08-19 23:18:40 +0000

    Fix part 101

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Sends a POST request with the contents of a file
-curl -X POST -H 'Content-Type: application/json' -d @"$2" "$1"
+curl -s -H 'Content-Type: application/json' -X POST "$1" -d @"$2"
