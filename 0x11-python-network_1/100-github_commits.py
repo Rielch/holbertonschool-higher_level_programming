83781b0eeda17d47f6fcb84425d2604f931ee951
Rielch <2288@holbertonschool.com>
2021-08-20 20:02:20 +0000

    Add part 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Returns the last 10 commits in a repository"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    r = requests.get('https://api.github.com/repos/{}/{}/commits?per_page=10'
+                     .format(sys.argv[2], sys.argv[1]))
+    http = r.json()
+    for element in http:
+        print('{}: {}'.format(element['sha'], element['commit']['author']['name']))
